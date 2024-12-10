4cf77cd4ff9d53faa036a19d4021e8b8ec255293
Math_rad_round <mathdaren@qq.com>
2024-12-10 18:58:59 +0800

    advance,release

--- client/src/components/pano/VRroute.js
+++ client/src/components/pano/VRroute.js
@@ -18,3 +18,4 @@ class VRroute extends Component{
 
 
 export default VRroute;
+
--- public/game/skin.js
+++ public/game/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.8/17956
 // Filename: testinfo.ggsk
-// Generated 2024-12-09T19:51:15
+// Generated 2024-12-10T18:58:45
 
 function pano2vrSkin(player,base) {
 	var me=this;
