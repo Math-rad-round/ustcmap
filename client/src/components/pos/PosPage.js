571196ecd5ca2a710f45e959afbca7e2b844928d
Math_rad_round <mathdaren@qq.com>
2025-12-12 17:41:39 +0800

    移动端测试2

--- client/src/components/pos/PosPage.js
+++ client/src/components/pos/PosPage.js
@@ -1,56 +1,56 @@
-import React , { Component }from "react";
-import { Link } from "react-router-dom";
-import { post,get } from "../../utilities.js";
-import Textbox from "../newmod/Textbox.js";
-import PosBox from "../posmod/PosBox.js";
-class PosPage extends Component{
-  constructor(props){
+import React, { Component } from 'react';
+
+class PosPage extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      latitude:"未定位",
-      longitude:"未定位",
-      accuracy:"未定义",
-      timestamp:"未定义",
+    this.state = {
+      location: null,
+      accuracy: null,
+      error: null,
     };
-    this.changexy= this.changexy.bind(this);
-    this.handleLocationUpdate= this.handleLocationUpdate.bind(this);
-    this.handleLocationError= this.handleLocationError.bind(this);
-    this.handleUseLocation= this.handleUseLocation.bind(this);
-  };
-  changexy=(pos)=>{
-    console.log(pos);
-    this.setState(pos);
   }
-  handleLocationUpdate = (location) => {
-    console.log('位置更新:', location);
-    // 精度达到10米以内才认为是有效的
-    if (location.isHighAccuracy) {
-      // 可以在地图上显示位置
-      // updateMap(location.latitude, location.longitude);
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // 获取经纬度和精度信息
+          const { latitude, longitude, accuracy } = position.coords;
+          this.setState({ location: { latitude, longitude }, accuracy });
+        },
+        (err) => {
+          this.setState({ error: '无法获取位置信息' });
+          console.error(err);
+        },
+        {
+          enableHighAccuracy: true, // 启用高精度
+          timeout: 10000, // 设置超时时间
+          maximumAge: 0, // 不使用缓存的定位信息
+        }
+      );
+    } else {
+      this.setState({ error: '此浏览器不支持地理位置功能' });
     }
-  };
+  }
 
-  handleLocationError = (error) => {
-    console.error('定位错误:', error);
-  };
-  handleUseLocation = (location) => {
-    alert(`使用位置: ${location.latitude}, ${location.longitude}`);
-  };
+  render() {
+    const { location, accuracy, error } = this.state;
 
-  render(){
     return (
       <div>
-      <PosBox
-        autoOptimize={true}
-        onLocationUpdate={this.handleLocationUpdate}
-        onLocationError={this.handleLocationError}
-        onUseLocation={this.handleUseLocation}
-      />
-    </div>
+        {error && <p>{error}</p>}
+        {location ? (
+          <div>
+            <p>纬度: {location.latitude}</p>
+            <p>经度: {location.longitude}</p>
+            <p>定位精度: {accuracy} 米</p> {/* 显示精度 */}
+          </div>
+        ) : (
+          <p>正在获取位置信息...</p>
+        )}
+      </div>
     );
   }
 }
-
-
 
 export default PosPage;
